af6d55b91628e45e1840ce648d55007f1fc3796a
CheolHoJung <fnel159@naver.com>
2018-08-27 11:02:23 +0900

    feat: Add Drink class

---
+++ VendingMachine/src/main/java/vending/machine/Drink.java
@@ -0,0 +1,13 @@
+package vending.machine;
+
+public class Drink {
+
+    String name;
+    
+    int price;
+
+    public Drink(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
