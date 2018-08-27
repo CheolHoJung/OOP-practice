1b7cdd42887ea7e3f0354d1473b0c8189813cada
CheolHoJung <fnel159@naver.com>
2018-08-28 00:22:46 +0900

    test: Add test cases about statistics * 통계_초기화 * 통계_추가 * 통계_수익률_5등_2개_4등_1개

--- Lotto/src/test/java/lotto/machine/LottoMachineTest.java
+++ Lotto/src/test/java/lotto/machine/LottoMachineTest.java
@@ -82,4 +82,35 @@ public class LottoMachineTest {
         LottoRank result = machine.raffle(userInputLottoTicket);
         assertEquals(LottoRank.NONE, result);
     }
+    
+    @Test
+    public void test_통계_초기화() throws Exception {
+        LottoStatistics statistics = new LottoStatistics();
+        assertEquals(0, statistics.size(LottoRank.FIRST));
+        assertEquals(0, statistics.size(LottoRank.SECOND));
+        assertEquals(0, statistics.size(LottoRank.THIRD));
+        assertEquals(0, statistics.size(LottoRank.FOURTH));
+        assertEquals(0, statistics.size(LottoRank.FIFTH));
+        assertEquals(0, statistics.size(LottoRank.NONE));
+    }
+    
+    @Test
+    public void test_통계_추가() throws Exception {
+        LottoStatistics statistics = new LottoStatistics();
+        statistics.add(LottoRank.FIRST, LottoTicket.generateByUserInput("1,2,3,4,5,6"));
+        statistics.add(LottoRank.FIRST, LottoTicket.generateByUserInput("1,2,3,4,5,6"));
+        assertEquals(2, statistics.size(LottoRank.FIRST));
+    }
+    
+    @Test
+    public void test_통계_수익률_5등_2개_4등_1개() throws Exception {
+        LottoStatistics statistics = new LottoStatistics();
+        statistics.add(LottoRank.FIFTH, LottoTicket.generateByUserInput("1,2,3,4,5,6"));
+        statistics.add(LottoRank.FIFTH, LottoTicket.generateByUserInput("1,2,3,4,5,6"));
+        statistics.add(LottoRank.FOURTH, LottoTicket.generateByUserInput("1,2,3,4,5,6"));
+        statistics.add(LottoRank.NONE, LottoTicket.generateByUserInput("1,2,3,4,5,6"));
+        statistics.add(LottoRank.NONE, LottoTicket.generateByUserInput("1,2,3,4,5,6"));
+        
+        assertEquals(1200, statistics.rateOfReturn());
+    }
 }
