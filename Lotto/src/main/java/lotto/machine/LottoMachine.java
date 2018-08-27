b86af57947bdc494738dbeb9973c1f65c5f34aa1
CheolHoJung <fnel159@naver.com>
2018-08-28 01:08:56 +0900

    feat: Edit LottoMachine to add LottoStatistics class

--- Lotto/src/main/java/lotto/machine/LottoMachine.java
+++ Lotto/src/main/java/lotto/machine/LottoMachine.java
@@ -1,6 +1,11 @@
 package lotto.machine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import lotto.number.LottoNumber;
 import lotto.rank.LottoRank;
@@ -11,6 +16,12 @@ public class LottoMachine {
     LottoTicket winNumberTicket;
     
     LottoNumber bonusNumber;
+    
+    LottoStatistics statistics;
+    
+    public LottoMachine() {
+        statistics = new LottoStatistics();
+    }
     
     public LottoRank raffle(LottoTicket targetLottoTicket) {
         Iterator<LottoNumber> winNumbersIterator = winNumberTicket.iterator();
@@ -28,7 +39,9 @@ public class LottoMachine {
             }
         }
         
-        return LottoRank.valueOf(count, targetLottoTicket.contains(bonusNumber));
+        LottoRank result = LottoRank.valueOf(count, targetLottoTicket.contains(bonusNumber));
+        statistics.add(result, targetLottoTicket);
+        return result;
     }
 
     public void setWinNumberTicket(LottoTicket winNumberTicket) {
@@ -37,5 +50,48 @@ public class LottoMachine {
 
     public void setBonusNumber(LottoNumber bonusNumber) {
         this.bonusNumber = bonusNumber;
+    }
+
+    public LottoStatistics statistics() {
+        return this.statistics;
+    }
+    
+    public static class LottoStatistics {
+
+        Map<LottoRank, List<LottoTicket>> board;
+        
+        public LottoStatistics() {
+            board = new HashMap<>();
+            board.put(LottoRank.FIRST, new ArrayList<>());
+            board.put(LottoRank.SECOND, new ArrayList<>());
+            board.put(LottoRank.THIRD, new ArrayList<>());
+            board.put(LottoRank.FOURTH, new ArrayList<>());
+            board.put(LottoRank.FIFTH, new ArrayList<>());
+            board.put(LottoRank.NONE, new ArrayList<>());
+        }
+        
+        public int size(LottoRank rank) {
+            return board.get(rank).size();
+        }
+        
+        void add(LottoRank rank, LottoTicket ticket) {
+            board.get(rank).add(ticket);
+        }
+
+        public int rateOfReturn() {
+            int inputMoney = (int) (board.values()
+                    .stream()
+                    .filter(l -> l.size() != 0)
+                    .flatMap(l -> l.stream())
+                    .count()) * LottoTicket.MONEY_PER_TICKET;
+            
+            int winMoney = board.keySet()
+                    .stream()
+                    .reduce(0,
+                        (acc, cur) -> acc + (cur.getPrice() * board.get(cur).size()),
+                        (i1, i2) -> i1 + i2);
+            
+            return winMoney / inputMoney * 100;
+        }
     }
 }
