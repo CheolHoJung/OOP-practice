ccbb81b268408e09a29cbdcf11fa34ba11be245c
CheolHoJung <fnel159@naver.com>
2018-09-05 10:08:21 +0900

    refactor: Edit ChangeModule.java to use coin enum class

--- VendingMachine/src/main/java/vending/machine/ChangeModule.java
+++ VendingMachine/src/main/java/vending/machine/ChangeModule.java
@@ -5,27 +5,20 @@ public class ChangeModule {
     public CoinSet getChangeCoinSet(int changeAmount) {
         CoinSet coinSet = new CoinSet();
         
-        while (changeAmount >= 500) {
-            changeAmount -= 500;
-            coinSet.add(500);
-        }
-        
-        while (changeAmount >= 100) {
-            changeAmount -= 100;
-            coinSet.add(100);
-        }
-        
-        while(changeAmount >= 50) {
-            changeAmount -= 50;
-            coinSet.add(50);
-        }
-        
-        while (changeAmount >= 10) {
-            changeAmount -= 10;
-            coinSet.add(10);
+        int remainChangeAmount = changeAmount;
+        for (Coin coin : Coin.values()) {
+            remainChangeAmount = addCoinsToCoinSet(remainChangeAmount, coinSet, coin);
         }
         
         return coinSet;
+    }
+
+    private int addCoinsToCoinSet(int changeAmount, CoinSet coinSet, Coin coin) {
+        while (coin.isLessThanOrEqualTo(changeAmount)) {
+            changeAmount = coin.changeAmount(changeAmount);
+            coinSet.add(coin);
+        }
+        return changeAmount;
     }
 
 }
