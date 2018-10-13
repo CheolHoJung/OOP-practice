d7afaf7800a336aba07a3ffccf53c0b3a4c7bdb6
CheolHoJung <fnel159@naver.com>
2018-10-13 21:42:51 +0900

    refactor: Remove implmenetation of Iterable Remove implementation of Iterable<LottoNumber>. And add constraint variables to regex in generateByUserInput method. And add getRatinLottoNumberCount method.

--- Lotto/src/main/java/lotto/ticket/LottoTicket.java
+++ Lotto/src/main/java/lotto/ticket/LottoTicket.java
@@ -1,22 +1,26 @@
 package lotto.ticket;
 
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import lotto.number.LottoNumber;
 
-public class LottoTicket implements Iterable<LottoNumber> {
+public class LottoTicket {
 
     public static final int MONEY_PER_TICKET = 1000;
+    public static final int MAX_SIZE = 6; 
+    
+    private static final String REGEX_TO_LOTTO_NUMBERS = "[\\[\\]]";
+    private static final String REGEX_TO_SPLIT = "[\\s,]+";
     
     Set<LottoNumber> lottoNumbers;
     
     public LottoTicket(Set<LottoNumber> lottoNumbers) {
         this.lottoNumbers = new TreeSet<>(lottoNumbers);
-        if (lottoNumbers.size() != 6) {
+        if (lottoNumbers.size() != MAX_SIZE) {
             throw new IllegalArgumentException("로또 용지 한장은 6개의 숫자여야 합니다. 현재 숫자: " + this.lottoNumbers);
         }
     }
@@ -27,15 +31,12 @@ public class LottoTicket implements Iterable<LottoNumber> {
     }
 
     public static LottoTicket generateByUserInput(String input) {
-        
-        Set<LottoNumber> lottoNumbers = Arrays.stream(input.replaceAll("[\\s\\[\\]]", "").split(","))
-                .map(s -> LottoNumber.valueOf(s))
+        String replace = input.replaceAll(REGEX_TO_LOTTO_NUMBERS, "");
+        String[] numbers = replace.split(REGEX_TO_SPLIT);
+        Set<LottoNumber> generatedLottoNumbers = Arrays.stream(numbers)
+                .map(number -> LottoNumber.valueOf(number))
                 .collect(Collectors.toSet());
-        return new LottoTicket(lottoNumbers);
-    }
-    
-    public Iterator<LottoNumber> iterator() {
-        return lottoNumbers.iterator();
+        return new LottoTicket(generatedLottoNumbers);
     }
     
     public boolean contains(LottoNumber number) {
@@ -44,5 +45,11 @@ public class LottoTicket implements Iterable<LottoNumber> {
     
     public static boolean hasSmallChange(int money) {
         return money % LottoTicket.MONEY_PER_TICKET != 0;
+    }
+    
+    public int getRetainLottoNumberCount(LottoTicket ticket) {
+        Set<LottoNumber> copy = new HashSet<>(lottoNumbers);
+        copy.retainAll(ticket.lottoNumbers);
+        return copy.size();
     }
 }
