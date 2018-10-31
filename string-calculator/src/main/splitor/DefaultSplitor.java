f9e4033847080d0ffd384d5c51746c2f65e7e917
CheolHoJung <fnel159@naver.com>
2018-10-31 19:17:38 +0900

    refactor: Remove unuse packages

--- string-calculator/src/main/splitor/DefaultSplitor.java
+++ string-calculator/src/main/splitor/DefaultSplitor.java
@@ -1,8 +1,6 @@
 package main.splitor;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class DefaultSplitor implements Splitor {
 
--- string-calculator/src/main/splitor/Splitor.java
+++ string-calculator/src/main/splitor/Splitor.java
@@ -1,7 +1,5 @@
 package main.splitor;
 
-import java.util.List;
-
 public interface Splitor {
     int[] split(String s);
 }
