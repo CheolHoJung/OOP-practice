27af5505f0db7e523769a27f1cbef2712c50d783
jcheolho <fnel159@naver.com>
2019-03-06 20:52:00 +0900

    feat: Add default account 'jcheolho.dev@gmail.com/jch'

--- inflearn-rest-api-event-application/src/main/java/jch/inflearn/springbootrest/configs/AppConfig.java
+++ inflearn-rest-api-event-application/src/main/java/jch/inflearn/springbootrest/configs/AppConfig.java
@@ -1,13 +1,24 @@
 package jch.inflearn.springbootrest.configs;
 
+import jch.inflearn.springbootrest.accounts.Account;
+import jch.inflearn.springbootrest.accounts.AccountRole;
+import jch.inflearn.springbootrest.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Set;
+
 @Configuration
 public class AppConfig {
+
+    @Autowired
+    AccountService accountService;
 
     @Bean
     public ModelMapper modelMapper() {
@@ -17,5 +28,14 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> accountService.save(Account.builder()
+                    .email("jcheolho.dev@gmail")
+                    .password("jch")
+                    .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                    .build());
     }
 }
