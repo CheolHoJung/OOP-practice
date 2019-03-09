f88c499fc4da712d9ded7225d91b94af56cfb59a
jcheolho <fnel159@naver.com>
2019-03-10 00:47:23 +0900

    test: Add oauth token test

---
+++ inflearn-rest-api-event-application/src/test/java/jch/inflearn/springbootrest/configs/AuthServerConfigTest.java
@@ -0,0 +1,49 @@
+package jch.inflearn.springbootrest.configs;
+
+import jch.inflearn.springbootrest.accounts.Account;
+import jch.inflearn.springbootrest.accounts.AccountRole;
+import jch.inflearn.springbootrest.accounts.AccountService;
+import jch.inflearn.springbootrest.common.BaseControllerTest;
+import jch.inflearn.springbootrest.common.TestDescription;
+import org.junit.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @TestDescription("인증 토큰을 발급 받는 테스트")
+    public void getAuthToken() throws Exception {
+        // Given
+        String username = "jcheolho,dev@gmail.com";
+        String password = "jch";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountService.save(account);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        this.mockMvc.perform(post("/oauth/token")
+                    .with(httpBasic(clientId, clientSecret))
+                    .param("username", username)
+                    .param("password", password)
+                    .param("grant_type", "password"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists());
+    }
+}
