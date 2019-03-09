be2a18f435e75da072a4963713434a124b569cd8
jcheolho <fnel159@naver.com>
2019-03-10 01:43:06 +0900

    test: Change test account 'jcheolho.dev@gmail.com/jch' to 'jcho.dev@gmail.com/jch

--- inflearn-rest-api-event-application/src/test/java/jch/inflearn/springbootrest/configs/AuthServerConfigTest.java
+++ inflearn-rest-api-event-application/src/test/java/jch/inflearn/springbootrest/configs/AuthServerConfigTest.java
@@ -25,7 +25,7 @@ public class AuthServerConfigTest extends BaseControllerTest {
     @TestDescription("인증 토큰을 발급 받는 테스트")
     public void getAuthToken() throws Exception {
         // Given
-        String username = "jcheolho,dev@gmail.com";
+        String username = "jcho@gmail.com";
         String password = "jch";
         Account account = Account.builder()
                 .email(username)
